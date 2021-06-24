3824233ac609981bd88444745aae4ebff394630c
lukakh1 <86411342+lukakh1@users.noreply.github.com>
2021-06-24 16:41:45 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
+import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
+
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELED: 2,
+};
+
+export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const addFocusHistorySubjectWithState = (subject, status) => {
+    setFocusHistory([...focusHistory, { subject: subject, status }]);
+  };
+
+  return (
+    <View style={styles.container}>
+      {focusSubject ? (
+        <Timer
+          focusSubject={focusSubject}
+          onTimerEnd={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.COMPLETE);
+            setFocusSubject(null);
+          }}
+          clearSubject={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.CANCELED);
+            setFocusSubject(null);
+          }}
+        />
+      ) : (
+        <Focus addSubject={setFocusSubject} />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "ios" ? spacing.md : spacing.lg,
+    backgroundColor: colors.darkBlue,
+  },
+});
